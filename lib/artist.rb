8be9c1dd69625df0a40513c05ac83ac5804c53d7
Amie Foster <amie.n.foster@gmail.com>
2021-10-14 19:02:08 -0600

    completed

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
-
 class Artist
+
   extend Findable
   include Paramable
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :songs
 
@@ -15,7 +16,7 @@ class Artist
     super
     @songs = []
   end
-  
+
   def self.all
     @@artists
   end
